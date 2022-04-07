143e3cf6958966adc8057449992715d0830d16ca
unknown <deadmarvald@gmail.com>
2022-04-07 04:35:25 +0300

    Лабораторна робота №3.1 ПКР

---
+++ UT3.1PKR/UT3.1PKR.cpp
@@ -0,0 +1,33 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../PR3.1PKR/Source.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UT31PKR
+{
+	TEST_CLASS(UT31PKR)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double t;
+			int N = 4;
+			Student* s = new Student[N];
+
+			s[0].math = 5;
+			s[0].physics = 5;
+			s[1].math = 4;
+			s[1].physics = 4;
+			s[2].math = 3;
+			s[2].physics = 4;
+			s[3].math = 2;
+			s[3].physics = 5;
+
+			t = LineSearch(s, N);
+
+			Assert::AreEqual(t, 50.0);
+		}
+	};
+}
